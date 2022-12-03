a1d748bcb94dfb521514abfd0be0ed732a39ed53
Kenjiro Yamada <76738596+y-kethub@users.noreply.github.com>
2022-12-03 18:35:20 +0900

    Initial Commit

---
+++ mdNOTE/mdNOTEApp.swift
@@ -0,0 +1,17 @@
+//
+//  mdNOTEApp.swift
+//  mdNOTE
+//
+//  Created by developer on 2022/12/03.
+//
+
+import SwiftUI
+
+@main
+struct mdNOTEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mdNOTEUITests/mdNOTEUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  mdNOTEUITestsLaunchTests.swift
+//  mdNOTEUITests
+//
+//  Created by developer on 2022/12/03.
+//
+
+import XCTest
+
+final class mdNOTEUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
